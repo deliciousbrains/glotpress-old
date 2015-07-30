055d8e344a001bd574ed83dd69fba4d4369ca11d
Ashley Rich <hello@ashleyrich.com>
2015-07-30 11:35:15 +0100

    Main plugin file added

---
+++ glotpress.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: GlotPress
+Plugin URI:  https://github.com/deliciousbrains/glotpress
+Description: GlotPress is a collaborative, web-based software translation tool.
+Version:     0.1
+Author:      Delicious Brains
+Author URI:  https://deliciousbrains.com
+Text Domain: glotpress
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+require_once( dirname( __FILE__ ) . '/gp-load.php' );
