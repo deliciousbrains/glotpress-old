7281ff6a2ebd0d6ee6e1e88ae94954a28e6c994c
Ashley Rich <hello@ashleyrich.com>
2015-07-31 15:23:43 +0100

    Plugin header updated

--- glotpress.php
+++ glotpress.php
@@ -1,24 +1,12 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
- * @since             1.0.0
- * @package           GlotPress
- *
- * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/glotpress-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       GlotPress
+ * Plugin URI:        https://github.com/deliciousbrains/glotpress
+ * Description:       GlotPress is a collaborative, web-based software translation tool.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            GlotPress Community
+ * Author URI:        https://github.com/deliciousbrains/glotpress
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       glotpress
